dfc70009e949cb61dfa755494f38dc71a89c4020
sv.goblinas <sv.goblinas@gmail.com>
2017-01-31 10:32:22 +0200

    labas

---
+++ pamoka/pamoka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pamoka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("labas");
+            Console.ReadLine();
+        }
+    }
+}
