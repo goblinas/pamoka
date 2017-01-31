7995459900f908c91398ec770d14d7045ff08156
sv.goblinas <sv.goblinas@gmail.com>
2017-01-31 12:22:54 +0200

    pint class

--- pamoka/pamoka/Program.cs
+++ pamoka/pamoka/Program.cs
@@ -10,7 +10,18 @@ namespace pamoka
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("labas pasauli kkkdjfdk");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 8;
+            p2.symb = '+';
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
